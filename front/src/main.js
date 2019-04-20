b7de4a95383cf109a98b22bff546ed1ecc7455eb
William Vitorino <williamvitorino3@gmail.com>
2019-04-20 12:47:51 -0300

    install and config axios + i18n + bootstrap.

--- front/src/main.js
+++ front/src/main.js
@@ -3,13 +3,34 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import VueI18n from 'vue-i18n'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+let i18n = new VueI18n(
+  {
+    locale: 'pt-br',
+    fallbackLocale: 'pt-br',
+    localeDir: '@/i18n',
+    enableInSFC: false
+  }
+);
+
 new Vue({
   el: '#app',
   router,
+  i18n,
   components: { App },
   template: '<App/>'
 })
