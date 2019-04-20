676ff6bda200e9c7f0a90fc03fbc1a7e1711687c
William Vitorino <williamvitorino3@gmail.com>
2019-04-20 15:48:51 -0300

    componente de left menu.

---
+++ front/src/components/LeftMenu.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <router-link class="link" v-for="(item, index) in links" :key="index" tag="div" :to="item.link">
+      <i :class="item.icon" class="icon" />
+      <!-- <font-awesome-icon :icon="item.icon" class="icon" /> -->
+      <span v-show="toShow">{{ item.label }}</span>
+    </router-link>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    toShow: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {
+    links() {
+      return [
+        {
+          label: this.$t('page.label.home'),
+          link: '/',
+          icon: 'fa fa-home'
+        },
+        {
+          label: this.$t('page.label.form'),
+          link: 'form',
+          icon: 'fab fa-wpforms'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+section {
+  margin-top: 0;
+}
+.icon {
+  margin-right: 1rem;
+}
+.link {
+  height: 4rem;
+  padding-left: 15px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+.link:hover {
+  background-color: #145987;
+}
+</style>
