04ef60ce6b43f313e44e66648fee2c26ed59db15
William Vitorino <williamvitorino3@gmail.com>
2019-04-20 15:46:57 -0300

    Configuração das dependências.

--- front/src/main.js
+++ front/src/main.js
@@ -3,34 +3,36 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
 import VueI18n from 'vue-i18n'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import BootstrapVue from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(VueAxios, axios)
+Vue.use(VueI18n)
+
+const messages = {
+  'pt-br': require('@/i18n/pt-br.json'),
+  'en-us': require('@/i18n/en-us.json')
+}
 
 Vue.config.productionTip = false
 
+const i18n = new VueI18n({
+  locale: 'pt-br',
+  fallbackLocale: 'en-us',
+  messages
+})
+
 /* eslint-disable no-new */
-
-let i18n = new VueI18n(
-  {
-    locale: 'pt-br',
-    fallbackLocale: 'pt-br',
-    localeDir: '@/i18n',
-    enableInSFC: false
-  }
-);
-
 new Vue({
   el: '#app',
-  router,
   i18n,
+  router,
   components: { App },
   template: '<App/>'
 })
