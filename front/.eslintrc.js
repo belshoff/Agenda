dd7d51341b774cff829c917cf1ea1c5fdadf81ee
William Vitorino <williamvitorino3@gmail.com>
2019-04-20 12:32:52 -0300

    front end init

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -1,54 +1,29 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
   root: true,
-
   parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module'
+    parser: 'babel-eslint'
   },
-
   env: {
-    browser: true
+    browser: true,
   },
-
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
-    '@vue/standard'
+    'plugin:vue/essential', 
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
   ],
-
   // required to lint *.vue files
   plugins: [
     'vue'
   ],
-
-  globals: {
-    'ga': true, // Google Analytics
-    'cordova': true,
-    '__statics': true
-  },
-
   // add your custom rules here
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
-    // allow paren-less arrow functions
-    'arrow-parens': 'off',
-    'one-var': 'off',
-    'prefer-promise-reject-errors': 'off',
-
-    'import/first': 'off',
-    'import/named': 'error',
-    'import/namespace': 'error',
-    'import/default': 'error',
-    'import/export': 'error',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/no-extraneous-dependencies': 'off',
-
-    // allow console.log during development only
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    // allow debugger during development only
+    // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
